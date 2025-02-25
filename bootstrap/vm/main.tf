299aed4aa2d61b622d72e8338d24967a494fbe6e
Karl Schriek <karl.schriek@bma.network>
2025-02-25 12:56:14 +0100

    Add "admin_ssh_public_key" variable

--- bootstrap/vm/main.tf
+++ bootstrap/vm/main.tf
@@ -87,7 +87,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = var.admin_ssh_public_key
   }
 
   source_image_reference {
