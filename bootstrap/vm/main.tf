7aacb314af59db8683b6a5f9326a89d409083638
Karl Schriek <karl.schriek@bma.network>
2025-02-25 14:36:43 +0100

    admin_ssh_public_key is required

--- bootstrap/vm/main.tf
+++ bootstrap/vm/main.tf
@@ -1,6 +1,3 @@
-
-
-
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
--- bootstrap/vm/variables.tf
+++ bootstrap/vm/variables.tf
@@ -6,6 +6,6 @@ variable "subscription_id" { }
 variable "owned_subscription_id" {  }
 variable "vnet_address_space" {default = ["10.0.0.0/22"]}
 variable "subnet_address_prefixes" { default = ["10.0.0.0/24"]}
-variable "admin_ssh_public_key" { default = file("~/.ssh/id_rsa.pub")}
+variable "admin_ssh_public_key" {}
 
 
