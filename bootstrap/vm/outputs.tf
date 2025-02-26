0c79e7f00d3944b7f19171a8f53f93f32e334fac
Karl Schriek <karl.schriek@bma.network>
2025-02-26 12:59:13 +0100

    output identity

--- bootstrap/vm/outputs.tf
+++ bootstrap/vm/outputs.tf
@@ -20,6 +20,10 @@ output service_principal_object_id {
   value = azurerm_virtual_network.vnet.id
  }
 
-  output "subnet_id" {
+output "subnet_id" {
   value = azurerm_subnet.subnet.id
+ }
+
+output "identity" {
+  value =  azurerm_linux_virtual_machine.vm.identity
  }
